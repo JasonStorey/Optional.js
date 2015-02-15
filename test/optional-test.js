392ce95888a8d22b43526b7224f0ed7f8877c62e
Jason Storey <jason.andrew.storey@gmail.com>
2015-02-15 23:45:58 +0000

    Implement .isPresent() and refactor

--- test/optional-test.js
+++ test/optional-test.js
@@ -3,6 +3,16 @@ var assert = require('assert'),
 	OptionalConstructor = require('../lib/optional.js');
 
 describe('Optional.js', function() {
+    var nonNullValue,
+        nullValue,
+        undefinedValue;
+
+    beforeEach(function() {
+        nonNullValue = 'a non-null value';
+        nullValue = null;
+        undefinedValue = undefined;
+    });
+
 	describe('implements static methods', function() {
 
 		it('.empty() returns an empty Optional instance with no set value', function() {
@@ -13,36 +23,29 @@ describe('Optional.js', function() {
     	});
 
 		it('.of() returns an Optional describing the specified non-null value', function() {
-			var nonNullValue = 'this is a non-null value',
-				optional = Optional.of(nonNullValue);
+			var optional = Optional.of(nonNullValue);
 
         	assert(optional instanceof OptionalConstructor);
         	assert.strictEqual(optional._value, nonNullValue);
     	});
 
 		it('.of() throws an exception if value is not defined', function() {
-			var undefinedValue;
-
         	assert.throws(function() { Optional.of(undefinedValue); }, /NullPointerException : value is not defined/);
     	});
 
 		it('.of() throws an exception if value is null', function() {
-			var nullValue = null;
-
         	assert.throws(function() { Optional.of(nullValue); }, /NullPointerException : value is not defined/);
     	});
 
     	it('.ofNullable() returns an Optional describing the specified non-null value', function() {
-			var nonNullValue = 'this is a non-null value',
-				optional = Optional.ofNullable(nonNullValue);
+			var optional = Optional.ofNullable(nonNullValue);
         	
         	assert(optional instanceof OptionalConstructor);
         	assert.strictEqual(optional._value, nonNullValue);
     	});
 
     	it('.ofNullable() returns an empty Optional when value is not defined', function() {
-			var undefinedValue,
-				optional = Optional.ofNullable(undefinedValue);
+			var optional = Optional.ofNullable(undefinedValue);
 
         	assert(optional instanceof OptionalConstructor);
         	assert.strictEqual(optional._value, undefined);
@@ -50,19 +53,30 @@ describe('Optional.js', function() {
 	});
 
     describe('implements instance methods', function() {
+        var emptyOptional;
+
+        beforeEach(function() {
+            emptyOptional = Optional.empty();
+        });
 
         it('.get() returns value if present in Optional', function() {
-            var nonNullValue = 'a non-null value',
-                optional = Optional.of(nonNullValue);
+            var nonNullOptional = Optional.of(nonNullValue);
 
-            assert.strictEqual(optional.get(), nonNullValue);
+            assert.strictEqual(nonNullOptional.get(), nonNullValue);
         });
 
         it('.get() throws an exception if Optional is empty', function() {
-            var emptyOptional = Optional.empty();
-
             assert.throws(function() { emptyOptional.get(); }, /NoSuchElementException : Optional is empty/);
         });
 
+        it('.isPresent() returns true if Optional contains value', function() {
+            var nonNullOptional = Optional.of(nonNullValue);
+
+            assert(nonNullOptional.isPresent());
+        });
+
+        it('.isPresent() returns false if Optional is empty', function() {
+            assert(!emptyOptional.isPresent());
+        });
     });
 });
